f35ac1677d84d983d2253377647440aba10458ce
dustinsimmons <Dustin.simmons@simplymac.com>
2022-03-18 12:14:45 -0400

    Term update

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ action = ActionChains(driver)
 global loop_range
 # list for configurations that will be appended after API call
 url_list = []
+url_list2 = []
 
 
 def config_api_call():
@@ -43,24 +44,36 @@ def config_api_call():
     # PrettyPrint displays an easy to read JSON object (or just nested dictionary objects)
     pp = pprint.PrettyPrinter(indent=4)
 
-    # To add parameters to API call, edit the following URL
-    # example: 'page[size]=1000' sets the amount of configurations to be returned from the call
+    ###################################################################################################################
+    # To add parameters to API call, edit the following URL                                                           #
+    # example: 'page[size]=1000' sets the amount of configurations to be returned from the call                       #
+    ###################################################################################################################
+    # At the time of this comment, there are 1134 configurations. To get around the 1000 result limit in IT Glue API, #
+    # split response into two variables that return 600 results each by adjusting the page size and page number       #
+    ###################################################################################################################
     response = requests.get(
-        'https://api.itglue.com/organizations/{0}/relationships/configurations?page[size]=1000'
+        'https://api.itglue.com/organizations/{0}/relationships/configurations?page[size]=600'
+        .format(org_id), headers=headers
+    )
+    response2 = requests.get(
+        'https://api.itglue.com/organizations/{0}/relationships/configurations?page[size]=600&page[number]=2'
         .format(org_id), headers=headers
     )
 
-    pp.pprint(response.json())
+    pp.pprint(response2.json())
 
     # return response as python json object and find length
     json_object = response.json()
     json_object_len = len(json_object['data'])
 
+    json_object2 = response2.json()
+    json_object_len2 = len(json_object2['data'])
+
     ###################################################################################################################
     # The API call returns each configuration including ALL attributes. For this script, the only important attribute #
     # to grab is the URL which will be appended to the 'url_list' list. Each URL will then be opened in the automated #
-    # Selenium browser window where they will be moved to the IT security group one by one due to the lack of bulk    #
-    # moves
+    # Selenium browser window where they will be moved to the IT security group one by one due to IT Glue not having  #
+    # the ability to perform bulk moves                                                                               #
     ###################################################################################################################
     # When appending to the list you see "['data'][counter]['attributes']['resource-url']" which is pathing out what  #
     # specific attribute I want appended which in this case is simply the URL of the configuration. The counter       #
@@ -73,8 +86,14 @@ def config_api_call():
         url_list.append(json_object['data'][counter]['attributes']['resource-url'])
         counter += 1
 
+    counter2 = 0
+    for i in range(json_object_len2):
+        url_list2.append(json_object2['data'][counter2]['attributes']['resource-url'])
+        counter2 += 1
+
     response.close()
-    return url_list
+    response2.close()
+    return url_list, url_list2
 
 
 def locationTable():
@@ -176,7 +195,7 @@ def configurations():
     ).click()
     print("Selected IT org")
     driver.implicitly_wait(10)
-    driver.find_element_by_xpath('//*[@id="react-main"]/div/div[3]/nav/ul/li[4]/a/div').click()
+    driver.find_element_by_xpath('/html/body/div[1]/section/div/div[1]/div/div[3]/nav/ul/li[4]/a/span[1]').click()
     driver.implicitly_wait(10)
     print("Navigated to Configurations")
 
@@ -187,15 +206,13 @@ def configurations():
 
     time.sleep(5)
 
-    print('\nNumber of Configurations:', len(url_list), '\n')
+    print('\nNumber of Configurations wave 1:', len(url_list))
+    print('\nNumber of Configurations wave 2:', len(url_list2), '\n')
 
     # Counters to report number of True/False blocks after script completes
     true_check = 0
     false_check = 0
 
-    ###################################################################################################################
-    # Unfortunately IT Glue has a maximum of 1000 pages (i.e. configurations being pulled)                            #
-    # This means that since there's greater than 1000 configurations, it's necessary to update in waves               #
     ###################################################################################################################
     # The list pulled from the API call isn't alphabetized like the website, however, it does seem to be in the same  #
     # order everytime meaning that updating in waves should not be a problem.                                         #
@@ -213,10 +230,86 @@ def configurations():
         # I have no idea why sleeping for 2 seconds fixed this whole block but don't remove it
         time.sleep(3)
 
+        ###############################################################################################################
+        # IMPORTANT: Notice that find_element(s) is used here instead of find_element like everywhere else            #
+        # This is because when find_elements is used and the element isn't found, an empty list is returned and       #
+        # when find_element is used and the element isn't found, it throws an exception                               #
+        ###############################################################################################################
+        # Click 'edit' button if it's found
+        if driver.find_elements_by_xpath(
+                '/html/body/div[1]/section/div/div/div/div[3]/div/div[2]/div[1]/div/div/div/a[1]'):
+            driver.find_element_by_xpath(
+                '/html/body/div[1]/section/div/div/div/div[3]/div/div[2]/div[1]/div/div/div/a[1]').click()
+            driver.implicitly_wait(60)
+
+        time.sleep(2)
+
+        # Click 'Specific Groups and/or Users can access this Configuration' radio button
+        driver.execute_script('arguments[0].click();', WebDriverWait(driver, 60).until(
+            EC.element_to_be_clickable((
+                By.XPATH, '/html/body/div[1]/section/div/div[2]/form/div[2]/div/div[1]/div[2]/label'))))
+
+        driver.implicitly_wait(10)
+
+        # Click 'View Groups' dropdown menu
+        driver.execute_script('arguments[0].click();', WebDriverWait(driver, 60).until(
+            EC.element_to_be_clickable((
+                By.XPATH, '/html/body/div[1]/section/div/div[2]/form/div[2]/div/div[2]/div/div/div[1]/div/div[2]'))))
+
+        driver.implicitly_wait(10)
+        time.sleep(1)
+
+        # if block to check if location needs to be added to IT group
+        driver.execute_script('arguments[0].click();', WebDriverWait(driver, 60).until(
+            EC.element_to_be_clickable((
+                By.XPATH, '/html/body/div[1]/section/div/div[2]/form/div[2]/div/div[1]/div[2]'))))
+
+        driver.implicitly_wait(10)
+
+        checker = driver.find_element_by_xpath(
+            '/html/body/div[1]/section/div/div[2]/form/div[2]/div/div[2]/div/div/div[1]/div/div[3]/span[1]/label/input'
+        )
+        if checker.is_selected():
+            print('|  Entering TRUE block.. |')
+            driver.close()
+            driver.switch_to.window(driver.window_handles[0])
+            true_check += 1
+            print('|  Leaving TRUE block..  |')
+            print(x + 1, '------------------------', x + 1, '\n', sep='')
+        else:
+            print('|  Entering FALSE block.. |')
+            driver.execute_script('arguments[0].click();', WebDriverWait(driver, 20).until(
+                EC.element_to_be_clickable((
+                    By.XPATH,
+                    '/html/body/div[1]/section/div/div[2]/form/div[2]/div/div[2]/div/div/div[1]/div/div[3]/span[1]/label/input'))))
+            driver.implicitly_wait(10)
+            driver.execute_script('arguments[0].click();', WebDriverWait(driver, 20).until(
+                EC.element_to_be_clickable((
+                    By.XPATH,
+                    '//input[@class="btn btn-success btn-lg"]'))))
+            driver.implicitly_wait(10)
+            driver.close()
+            driver.switch_to.window(driver.window_handles[0])
+            false_check += 1
+            print('|  Leaving False block..  |')
+            print(x + 1, '------------------------', x + 1, '\n', sep='')
+
+    for x in range(len(url_list2)):
+        print(x + 1, '------------------------', x + 1, sep='')
+
+        # Open browser tab for each configuration URL in the list from the API call
+        driver.execute_script('''window.open("{0}");'''.format(url_list2[x]))
+
+        # Switch focus from original tab to newly opened configuration URL
+        driver.switch_to.window(driver.window_handles[1])
+        driver.implicitly_wait(10)
+        # I have no idea why sleeping for 2 seconds fixed this whole block but don't remove it
+        time.sleep(3)
+
         # Click 'edit' button
         driver.find_element_by_xpath(
             '/html/body/div[1]/section/div/div/div/div[3]/div/div[2]/div[1]/div/div/div/a[1]').click()
-        driver.implicitly_wait(10)
+        driver.implicitly_wait(60)
 
         time.sleep(2)
 
@@ -269,6 +362,7 @@ def configurations():
             false_check += 1
             print('|  Leaving False block..  |')
             print(x + 1, '------------------------', x + 1, '\n', sep='')
+
 
     # Timer to track runtime and print run statistics
     toc = time.perf_counter()
